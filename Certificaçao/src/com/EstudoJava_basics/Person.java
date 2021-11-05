44a0f6f6884ddb1881625e2da89542d8b0a5856b
Bruno Lopes Correia de Souza <bruno.lc@outlook.com.br>
2021-11-05 16:20:02 -0300

    Estudo para certificação

---
+++ Certificaçao/src/com/EstudoJava_basics/Person.java
@@ -0,0 +1,16 @@
+package com.EstudoJava_basics;
+
+public class Person {
+
+    public int x = 0;
+    public static int y = 0;
+
+    public static void setY(int y){
+        Person.y = y;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+
+
+}
