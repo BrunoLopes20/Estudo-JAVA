b20bb7337dddd8150277953a32ab7c784631da4b
Bruno Lopes Correia de Souza <bruno.lc@outlook.com.br>
2021-11-05 21:13:52 -0300

    .

--- EstruturaDeDados/src/com/EstudoJava_basics/Main.java
+++ EstruturaDeDados/src/com/EstudoJava_basics/Main.java
@@ -3,6 +3,20 @@ package com.EstudoJava_basics;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Criando e inicialziando o vetor
+        Vetor v = new Vetor(10);
+
+        //adicionando elementos no vetor
+        v.adicionar("BOLA");
+        v.adicionar("LÁPIS");
+        v.adicionar("LIVRO");
+        v.adicionar("PAPEL");
+        v.adicionar("FONE");
+
+        //saber o tamanho do vetor
+        System.out.println(v.getTamanho());
+
+
     }
 }
--- EstruturaDeDados/src/com/EstudoJava_basics/Vetor.java
+++ EstruturaDeDados/src/com/EstudoJava_basics/Vetor.java
@@ -2,23 +2,44 @@ package com.EstudoJava_basics;
 
 public class Vetor {
 
-    private String[] nome;
-    private int tamanho;
+    private String[] elementos;
+    private int tamanho;//tamanho real de elementos dentro do array
 
 
     public Vetor(int capacidade){
-        this.nome = new String[capacidade];
-        
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
     }
 
-
-
-    public String[] getNome() {
-        return nome;
+    //adiciona um elemento no final da fila
+    public boolean adicionar(String elemento){
+        if(this.tamanho < this.elementos.length){
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
     }
 
-    public void setNome(String[] nome) {
-        this.nome = nome;
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        for(int i=0; i <this.elementos-1; i++){
+            s.append(this.elementos[i]);
+            s.append(",");
+        }
+        if(this.tamanho>0) {
+            s.append(this.elementos[this.tamanho - 1]);}
+        s.append("]");
+        return null;
+    }
+
+    public String[] getElementos() {
+        return elementos;
+    }
+
+    public void setElementos(String[] elementos) {
+        this.elementos = elementos;
     }
 
     public int getTamanho() {
