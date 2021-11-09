b47f7fa0220c8ec5a4604d595dc290ac42f2c50a
Bruno Lopes Correia de Souza <bruno.lc@outlook.com.br>
2021-11-08 21:06:32 -0300

    Começo projeto FullStack

---
+++ FullStackDevelopment/FullStackDevelop/src/test/java/com/java_angular/fullstackdevelop/FullStackDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_angular.fullstackdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
